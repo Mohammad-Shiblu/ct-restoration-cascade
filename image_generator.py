9b49b6a2b75acdb290c64da3d2fd33c354925482
Shiblu <mohammad.shiblu@fau.de>
2024-10-09 00:26:51 +0200

    code updated for test and val generation

--- image_generator.py
+++ image_generator.py
@@ -2,13 +2,13 @@ import numpy as np
 from PIL import Image
 import os
 
-def image_generator(size= (256, 256), no_images = 500, directory= "synthetic_data/images/"):
+def image_generator(size= (256, 256), no_images = 500, directory= "synthetic_data/images/", mode = 'L'):
     if not os.path.exists(directory):
         os.makedirs(directory)
     for i in range(no_images):
         image_array = np.random.randint(0, 255, size, dtype=np.uint8)   # np.random.normal(mean, stddev, size).clip(0, 255).astype(np.uint8) 
         img = Image.fromarray(image_array, mode='L')
-        img.save(f"synthetic_data/images/img_{i}.png")
+        img.save(f"{directory}/img_{i}.png")
 
 
 def add_gaussian_noise(image_source, target_source): 
@@ -30,7 +30,16 @@ def add_gaussian_noise(image_source, target_source):
         noisy_image.save(output_path)
 
 if __name__ == '__main__':
-    image_generator()
-   
-    add_gaussian_noise("synthetic_data/images/", "synthetic_data/noisy_image/")
-    
+    # image for training
+    SIZE = (256, 256)
+
+    image_generator(size=SIZE, no_images= 1000, directory="synthetic_data/train/images")
+    add_gaussian_noise("synthetic_data/train/images/", "synthetic_data/train/noisy_image/")
+
+    # Image for validation
+    image_generator(size=SIZE, no_images=200, directory="synthetic_data/val/images")
+    add_gaussian_noise("synthetic_data/val/images/", "synthetic_data/val/noisy_image/")
+
+    # Image for testing
+    image_generator(size=SIZE, no_images=200, directory="synthetic_data/test/images")
+    add_gaussian_noise("synthetic_data/test/images/", "synthetic_data/test/noisy_image/")
