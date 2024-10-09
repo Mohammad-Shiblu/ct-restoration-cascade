c837682b4d520bfd03a9b687c7852a057f391e03
Shiblu <mohammad.shiblu@fau.de>
2024-10-09 07:11:53 +0200

    datatset path mismatched solved

--- utils/dataset.py
+++ utils/dataset.py
@@ -8,20 +8,19 @@ class ImageDataset(Dataset):
         self.image_dir = image_dir
         self.target_dir = target_dir
         self.transform = transform
-        self.images  = os.listdir(image_dir)
+        self.images  = os.listdir(target_dir) # clean image
 
     def __len__(self):
         return len(self.images)
     
     def __getitem__(self, index):
-        img_path = os.path.join(self.image_dir, self.images[index])
-        target_path = os.path.join(self.target_dir, f"noise_{self.images[index]}")
+        target_path = os.path.join(self.target_dir, self.images[index])         # target path (clean image)
+        img_path = os.path.join(self.image_dir, f"noise_{self.images[index]}")  # input image (noisy image)
         image = Image.open(img_path).convert("L")
         target_image = Image.open(target_path).convert("L")
 
         if self.transform is not None:
-            transformation = self.transform(image = image, target = target_image)
-            image = transformation["image"]
-            target_image = transformation["target"]
+            image = self.transform(image)
+            target_image = self.transform(target_image) 
             
         return image, target_image
