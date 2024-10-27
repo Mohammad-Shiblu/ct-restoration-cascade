ceb5f55004124d84d2cf87d3f912cb7ed1ab42ff
Shiblu <mohammad.shiblu@fau.de>
2024-10-27 14:35:04 +0100

    noise generation process updated

--- utils/image_generator.py
+++ utils/image_generator.py
@@ -3,6 +3,7 @@ from PIL import Image
 import os
 import shutil
 import matplotlib.pyplot as plt
+from skimage.util import random_noise
 
 def image_selector(source_dir = "synthetic_data/imagewoof2/train/", target_dir= "synthetic_data/train/", size= 1000):
     if not os.path.exists(source_dir):
@@ -20,6 +21,7 @@ def image_selector(source_dir = "synthetic_data/imagewoof2/train/", target_dir= 
                     if width > 256 and height > 256:  # Only select images larger than (256, 256)
                         image_paths.append(img_path)
 
+    np.random.shuffle(image_paths)
     selected_image = np.random.choice(image_paths, size= size, replace=False)
     for image_path in selected_image:
         file_name = os.path.basename(image_path)
@@ -29,24 +31,64 @@ def image_selector(source_dir = "synthetic_data/imagewoof2/train/", target_dir= 
     print(f"{len(selected_image)} iamges saved to {target_dir}")
     return image_path
 
-def add_gaussian_noise(image_source, target_source): 
+
+# function for generating blcak and white patches in the image
+def add_patches(image, num_patches = 5, size_range = (10, 40)):
+    patched_image = image.copy()
+    height, width, channel = patched_image.shape
+    num = np.random.randint(0, num_patches)
+    for _ in range(num):
+        patch_height = np.random.randint(size_range[0], size_range[1])
+        patch_width = np.random.randint(size_range[0], size_range[1])
+
+        top_left_y = np.random.randint(0, height -patch_height)
+        top_left_x = np.random.randint(0, width - patch_width)
+
+        if np.random.rand() > 0.5:
+            patch_color = (0, 0 ,0)
+        else:
+            patch_color = (255, 255, 255)
+        
+        patched_image[top_left_y: top_left_y + patch_height, top_left_x: top_left_x + patch_width] = patch_color
+
+    return patched_image
+
+def add_noise(image, mean = 0, var = 0.2):
+    noisy_image  = random_noise(image, mode='gaussian', mean = mean, var=var)
+    noisy_image = (noisy_image * 255).astype(np.uint8)
+    return noisy_image
+
+def generate_noisy_dataset(image_source, target_source): 
     if not os.path.exists(target_source):
         os.makedirs(target_source)
-   
+    
     for filename in os.listdir(image_source):
         img_path = os.path.join(image_source, filename)
-        img = np.array(Image.open(img_path).convert('RGB')) 
+        img = np.array(Image.open(img_path).convert('RGB'))  # img
 
-        mean = np.random.uniform(0, 40)
-        std = np.random.uniform(10, 50)
-
-        noise = np.random.normal(mean, std, img.shape)
-        noisy_image = img + noise
-        noisy_image = np.clip(noisy_image, 0, 255).astype(np.uint8)
+        patched_image = add_patches(img)
+        noised_image = add_noise(patched_image)
         
-        noisy_image = Image.fromarray(noisy_image)
+        noised_image = Image.fromarray(noised_image)
         output_path = os.path.join(target_source, f"noise_{filename}")
-        noisy_image.save(output_path)
+        noised_image.save(output_path)
+
+def plot_image(clean_image_dir = "synthetic_data/train/clean_images/"):
+    clean_image_list = os.listdir(clean_image_dir)
+    np.random.seed(42)
+    rand_image = np.random.choice(clean_image_list)
+    clean_image_path = os.path.join(clean_image_dir, rand_image)
+    clean_image = Image.open(clean_image_path)
+    clean_image = np.array(clean_image)
+    patched_image = add_patches(clean_image)
+    noised_image = add_noise(patched_image)
+    plt.subplot(1, 2, 1)
+    plt.imshow(clean_image)
+    plt.axis('off')
+    plt.subplot(1, 2, 2)
+    plt.imshow(noised_image)
+    plt.axis('off')
+    plt.show()
 
 
 def show_image(clean_image_dir = "synthetic_data/train/clean_images/", noisy_image_dir = "synthetic_data/train/noisy_images/"):
@@ -70,11 +112,14 @@ def show_image(clean_image_dir = "synthetic_data/train/clean_images/", noisy_ima
 
 if __name__ == '__main__':
     # train image
-    image_selector(source_dir="synthetic_data/imagewoof2/train/", target_dir="synthetic_data/train/clean_images/", size=1000)
-    add_gaussian_noise("synthetic_data/train/clean_images/", "synthetic_data/train/noisy_images/")
+    # image_selector(source_dir="synthetic_data/imagewoof2/val/", target_dir="synthetic_data/val/clean_images/", size=200)
+
+    #add_noise("synthetic_data/train/clean_images/", "synthetic_data/train/noisy_images/")
+    
+    # plot_image()
     # # validation image
     # image_selector(source_dir="synthetic_data/imagewoof2/val/", target_dir="synthetic_data/val/clean_images/", size=200)
-    # add_gaussian_noise("synthetic_data/val/clean_images/", "synthetic_data/val/noisy_images/")
+    generate_noisy_dataset("synthetic_data/test/clean_images/", "synthetic_data/test/noisy_images/")
 
     # show_image()
     
